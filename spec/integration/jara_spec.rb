8f0f4ca4a0904623b8a22febc5f4ae3ea27ce296
Theo <theo@iconara.net>
2015-02-11 08:24:34 +0100

    Rephrase the integration tests and run them only in JRuby
    I'm trying to get things working in MRI too, for building tarballs

--- spec/integration/jara_spec.rb
+++ spec/integration/jara_spec.rb
@@ -64,99 +64,103 @@ describe 'Jara' do
     setup_git(@test_project_dir)
   end
 
-  context 'packaging the project as a JAR file that' do
-    let :jar_path do
-      File.expand_path(Dir["#{@test_project_dir}/build/production/test_project-*.jar"].first)
-    end
+  if defined? JRUBY_VERSION
+    context 'when creating a self-contained JAR' do
+      context 'the archive' do
+        let :jar_path do
+          File.expand_path(Dir["#{@test_project_dir}/build/production/test_project-*.jar"].first)
+        end
 
-    let :jar do
-      Java::JavaUtilJar::JarFile.new(Java::JavaIo::File.new(jar_path))
-    end
+        let :jar do
+          Java::JavaUtilJar::JarFile.new(Java::JavaIo::File.new(jar_path))
+        end
 
-    let :jar_entries do
-      jar.entries.to_a.map(&:name)
-    end
+        let :jar_entries do
+          jar.entries.to_a.map(&:name)
+        end
 
-    before :all do
-      run_package(@test_project_dir)
-    end
+        before :all do
+          run_package(@test_project_dir)
+        end
 
-    it 'has a name that includes the SHA of the master branch' do
-      sha = isolated_run(@test_project_dir, 'git rev-parse master').strip
-      jar_path.should include(sha[0, 8])
-    end
+        it 'has a name that includes the SHA of the master branch' do
+          sha = isolated_run(@test_project_dir, 'git rev-parse master').strip
+          jar_path.should include(sha[0, 8])
+        end
 
-    it 'includes the project files' do
-      jar_entries.should include('META-INF/app.home/lib/test_project.rb')
-    end
+        it 'includes the project files' do
+          jar_entries.should include('META-INF/app.home/lib/test_project.rb')
+        end
 
-    it 'includes the dependencies' do
-      jar_entries.grep(%r{META-INF/gem.home/paint-[^/]+/lib/paint.rb}).should_not be_empty
-    end
+        it 'includes the dependencies' do
+          jar_entries.grep(%r{META-INF/gem.home/paint-[^/]+/lib/paint.rb}).should_not be_empty
+        end
 
-    it 'does not include dependencies from groups other than the default' do
-      jar_entries.grep(/puck/).should be_empty
-    end
+        it 'does not include dependencies from groups other than the default' do
+          jar_entries.grep(/puck/).should be_empty
+        end
 
-    it 'includes JRuby' do
-      jar_entries.should include('org/jruby/Ruby.class')
-    end
+        it 'includes JRuby' do
+          jar_entries.should include('org/jruby/Ruby.class')
+        end
 
-    it 'can run the files in bin' do
-      output = isolated_run(Dir.tmpdir, %|java -jar #{jar_path} check|)
-      output.should include('Hello from check')
-      output = isolated_run(Dir.tmpdir, %|java -jar #{jar_path} main|)
-      output.should include("Hello from main \e[31m52\e[0m\n")
-    end
-  end
+        it 'can run the files in bin' do
+          output = isolated_run(Dir.tmpdir, %|java -jar #{jar_path} check|)
+          output.should include('Hello from check')
+          output = isolated_run(Dir.tmpdir, %|java -jar #{jar_path} main|)
+          output.should include("Hello from main \e[31m52\e[0m\n")
+        end
+      end
 
-  context 'packaging the project when the latest version has not been pushed' do
-    it 'fails the build' do
-      isolated_run(@test_project_dir, 'touch foobaz', 'git add foobaz', 'git commit -m "foobaz the fizzbuzz"')
-      expect { run_package(@test_project_dir) }.to raise_error(%r{master and origin/master are not in sync})
-    end
-  end
+      context 'when the latest version has not been pushed' do
+        it 'fails the build' do
+          isolated_run(@test_project_dir, 'touch foobaz', 'git add foobaz', 'git commit -m "foobaz the fizzbuzz"')
+          expect { run_package(@test_project_dir) }.to raise_error(%r{master and origin/master are not in sync})
+        end
+      end
 
-  context 'packaging the project for staging' do
-    let :jar_path do
-      File.expand_path(Dir["#{@test_project_dir}/build/staging/test_project-staging-*.jar"].first)
-    end
+      context 'when the branch is "staging"' do
+        let :jar_path do
+          File.expand_path(Dir["#{@test_project_dir}/build/staging/test_project-staging-*.jar"].first)
+        end
 
-    before :all do
-      isolated_run(@test_project_dir, 'git checkout -b staging', 'echo "puts \"Hello staging\"" > bin/check', 'git add .', 'git commit -m "Change the check message"', 'git push -u origin staging')
-      run_package(@test_project_dir, 'staging')
-    end
+        before :all do
+          isolated_run(@test_project_dir, 'git checkout -b staging', 'echo "puts \"Hello staging\"" > bin/check', 'git add .', 'git commit -m "Change the check message"', 'git push -u origin staging')
+          run_package(@test_project_dir, 'staging')
+        end
 
-    it 'includes the SHA of the staging branch in the artifact name' do
-      sha = isolated_run(@test_project_dir, 'git rev-parse staging').strip
-      jar_path.should include(sha[0, 8])
-    end
+        it 'includes the SHA of the staging branch in the artifact name' do
+          sha = isolated_run(@test_project_dir, 'git rev-parse staging').strip
+          jar_path.should include(sha[0, 8])
+        end
 
-    it 'uses the staging branch' do
-      output = isolated_run(Dir.tmpdir, %|java -jar #{jar_path} check|)
-      output.should include('Hello staging')
-    end
+        it 'uses the staging branch' do
+          output = isolated_run(Dir.tmpdir, %|java -jar #{jar_path} check|)
+          output.should include('Hello staging')
+        end
 
-    it 'uses the staging branch even when currently on another branch' do
-      isolated_run(@test_project_dir, 'git checkout master')
-      output = isolated_run(Dir.tmpdir, %|java -jar #{jar_path} check|)
-      output.should include('Hello staging')
-    end
-  end
+        it 'uses the staging branch even when currently on another branch' do
+          isolated_run(@test_project_dir, 'git checkout master')
+          output = isolated_run(Dir.tmpdir, %|java -jar #{jar_path} check|)
+          output.should include('Hello staging')
+        end
+      end
 
-  context 'packaging a test artifact from the working directory' do
-    let :jar_path do
-      File.expand_path("#{@test_project_dir}/build/test_project.jar")
-    end
+      context 'from the working directory' do
+        let :jar_path do
+          File.expand_path("#{@test_project_dir}/build/test_project.jar")
+        end
 
-    before :all do
-      isolated_run(@test_project_dir, 'echo "puts \"Hello test\"" > bin/check')
-      run_package(@test_project_dir, 'test')
-    end
+        before :all do
+          isolated_run(@test_project_dir, 'echo "puts \"Hello test\"" > bin/check')
+          run_package(@test_project_dir, 'test')
+        end
 
-    it 'includes unstaged changes' do
-      output = isolated_run(Dir.tmpdir, %|java -jar #{jar_path} check|)
-      output.should include('Hello test')
+        it 'includes unstaged changes' do
+          output = isolated_run(Dir.tmpdir, %|java -jar #{jar_path} check|)
+          output.should include('Hello test')
+        end
+      end
     end
   end
 end
